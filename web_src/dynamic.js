77fe4b77e9bfc820b69bf8babde145f48b497ac3
Lei Zuo <lzuo98@gmail.com>
2019-09-28 00:39:43 +0800

    nice

---
+++ web_src/dynamic.js
@@ -0,0 +1,23 @@
+function goinfo() {
+	window.location.href = './info.html';
+}
+
+
+function goprogress() {
+	window.location.href = './progress.html';
+}
+
+
+function godocumentation() {
+	window.location.href = './documentation.html';
+}
+
+
+function golinks() {
+	window.location.href = './links.html';
+}
+
+
+function goback() {
+	window.location.href = './dynamic.html';
+}
