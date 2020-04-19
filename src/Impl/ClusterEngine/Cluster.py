d689bed779d26586877faf8366c7070b05a40c43
imon247 <760250913@qq.com>
2020-04-19 21:48:41 +0800

    preliminary implementation of Nested Cluster

--- src/Impl/ClusterEngine/Cluster.py
+++ src/Impl/ClusterEngine/Cluster.py
@@ -2,10 +2,12 @@ from Base.SetBase import SetBase
 from Impl.DataCache import DataPoint as DP
 from typing import Set
 class Cluster(object):
-    def __init__(self, p:DP, points):
+    def __init__(self, c:DP, points):
         super().__init__()
-        self.center : DP = p
+        self.center : DP = c
         self.points : Set[DP] = points if points is not None else set()
+        self.rim = max(self.points, key=lambda p : p.sphereDist(self.center)) if len(self.points)>0 else None
+        self.radius = self.rim.sphereDist(self.center) if self.rim is not None else 0
 
     def __repr__(self):
         return "Cluster with center %s, points: %s" % (self.center, self.points)
@@ -13,15 +15,26 @@ class Cluster(object):
     # insert a point to the cluster
     def insert(self,p):
         self.points.add(p)
+        if self.radius < self.center.sphereDist(p):
+            self.rim = p
+            self.radius = self.center.sphereDist(p)
     
     # check if a point is in the cluster, cost: O(1)
     def contains(self,pid):
-        if DP.DataPoint(pid,-1,-1) in self.points:
-            return True
-        return False
+        # if DP.DataPoint(pid,-1,-1) in self.points:
+            # return True
+        # return False
+        return DP.DataPoint(pid,-1,-1) in self.points
 
     def remove(self,pid):
         self.points.discard(DP.DataPoint(pid,-1,-1))
+
+        # for p in self.points:
+        #     if p.id == pid:
+        #         self.points.discard(p)
+        #         if p.sphereDist(self.center)>=self.radius:
+        #             self.radius = max([p.sphereDist(self.center) for p in self.points]) if len(self.points)>0 else 0
+        #         break
     
     def nextCenter(self) -> DP:
         minDist = float("inf")
@@ -30,7 +43,7 @@ class Cluster(object):
             if p.sphereDist(self.center) < minDist:
                 minDist = p.sphereDist(self.center)
                 c = p
-        print('closest point is: ', c)
+        # print('closest point is: ', c)
         return c
     
     
--- src/start.py
+++ src/start.py
@@ -2,6 +2,7 @@ from Impl.ClusterEngine.DataStructure import DataStructure
 from Impl.DataSource.DataRetriever import DataRetriver
 from Impl.DataCache.Cache import Cache
 from Impl.Stability import HammingDist as Ham
+from Impl.Stability.Consistency import *
 from Impl.Visualization.test import Plotter
 import json
 import multiprocessing as mp
@@ -20,23 +21,23 @@ def main():
     print("finish clustering. Radius: ", dataStructure.radius)
     print(dataStructure.centers, "\ndone")
     dummy = input()
-    if config['recluster-method']==1:
-        testCf(dataStructure, action, cache)
-    elif config['calc-stab']==True and config['visualize']==True:
-        processCalcVsl(action, dataStructure, cache, config['draw-interval'])
-    elif config['calc-stab']==True and config['visualize']==False:
-        processCalc(action, dataStructure, cache)
-    elif config['calc-stab']==False and config['visualize']==True:
-        processVsl(action, dataStructure, cache, config['draw-interval'])
-    else:
-        processSld(action, dataStructure, cache)
+    if config['recluster-method']==0:
+        if config['calc-stab']==True and config['stab-measure']==0:
+            processCalc(action, dataStructure, cache)
+        elif config['calc-stab']==True and config['stab-measure']==1:
+            processCst(action, dataStructure, cache)
+        elif config['visualize']==True:
+            processVsl(action, dataStructure, cache, config['draw-interval'])
+    elif config['recluster-method']==1:
+        processCf(dataStructure, action, cache)
+
 
 
 
 def tryCluster(dmin:float, dmax:float, sigma:float, k:int, X:set):
 	i = 1
 	r = pow(1+sigma, i)
-	while r < dmin:
+	while r < 2000:
 		i += 1
 		r = pow(1+sigma, i)
 	while r <= dmax:
@@ -71,7 +72,7 @@ def processVsl(action, L:DataStructure, cache:Cache, d:int):
     i = 0
     while True:
         if i%d==0:
-            print("Sending centers1:", L.centers)
+            # print("Sending centers1:", L.centers)
             pl.plot(L)
         i+=1
         next(action)
@@ -85,6 +86,7 @@ def processVsl(action, L:DataStructure, cache:Cache, d:int):
         
 
 def processCalc(action, L:DataStructure, cache:Cache):
+    f = open("output", "w")
     while True:
         next(action)
         if cache.removed in [c.id for c in L.centers]:
@@ -92,16 +94,15 @@ def processCalc(action, L:DataStructure, cache:Cache):
             L.insert(cache.inserted)
             L.delete(cache.removed)
             c2 = L.refineForHam()
-            # print(L.Clusters)
             stab = Ham.ari(c1, c2)
-            if(stab<1):
-                print('Removed:', cache.removed, 'Inserted:', cache.inserted,)
-                print('stab:%.8f' % stab)
-                print(L.centers)
+            f.write('Removed: %s, Inserted: %s\n' % (cache.removed, cache.inserted))
+            f.write('stab:%.8f\n' % stab)
+            f.flush()
         else:
             L.insert(cache.inserted)
             L.delete(cache.removed)
         # dummy = input()
+    f.close()
 
 def processSld(action, L:DataStructure, cache:Cache):
     while True:
@@ -111,25 +112,45 @@ def processSld(action, L:DataStructure, cache:Cache):
         print('Removed:', cache.removed, 'Inserted:', cache.inserted)
         dummy = input()
 
-def testCf(L:DataStructure, action, cache):
-    # print(L.centers)
-    # print([len(cluster.points) for cluster in L.Clusters])
-    # print('')
+def processCf(L:DataStructure, action, cache):
+    f = open('output', 'w')
     while True:
         next(action)
         if cache.removed in [c.id for c in L.centers]:
-            print('Removed:', cache.removed, 'Inserted:', cache.inserted)
+            # print('Removed:', cache.removed, 'Inserted:', cache.inserted)
             c1 = L.refineForHam()
             L.insert(cache.inserted)
             L.cfDelete(cache.removed)
             c2 = L.refineForHam()
-            print(L.centers)
-            # print([len(cluster.points) for cluster in L.Clusters])
-            print('stab:%.8f'%Ham.ari(c1,c2))
-            # dummy = input()
+            stab = Ham.ari(c1, c2)
+            f.write('Removed: %s, Inserted: %s\n' % (cache.removed, cache.inserted))
+            f.write('stab:%.8f\n' % stab)
+            f.flush()
         else:
             L.insert(cache.inserted)
             L.cfDelete(cache.removed)
+    f.close()
+
+def processCst(action, L:DataStructure, cache:Cache):
+    f = open("output", "w")
+    T = 0
+    cumu = 0
+    while True:
+        next(action)
+        if cache.removed in [c.id for c in L.centers]:
+            centerSet1 = set(c.id for c in L.centers)
+            L.insert(cache.inserted)
+            L.delete(cache.removed)
+            centerSet2 = set(c.id for c in L.centers)
+            consistency = len(centerSet2.difference(centerSet1))
+            cumu += consistency
+            f.write("time T: %d, center diff: %d, cumulative diff: %d\n" % (T, consistency, cumu))
+            f.flush()
+        else:
+            L.insert(cache.inserted)
+            L.delete(cache.removed)
+        T += 1   
+    f.close()
 
 
 if __name__ == "__main__":
